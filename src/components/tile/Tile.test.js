57af1e0bd607b133a81ad58c2128cc72042fa6c9
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-08-12 12:38:28 +0100

    Create Tile tests

---
+++ src/components/tile/Tile.test.js
@@ -0,0 +1,11 @@
+const mockData = {
+  title: "Mock Appointment Title",
+  date: "1st January 1970",
+  time: "6am"
+}
+
+test.todo('Displays all object contents')
+
+test.todo('Gives first item class "tile-title"')
+
+test.todo('Gives non-first items class "tile"')
