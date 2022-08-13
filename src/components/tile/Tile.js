8e1475188b0ffa00d9f3f72d47440fa079dfc942
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-08-13 11:40:55 +0100

    Implement first test

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({display}) => {
+  const entries = Object.entries(display);
+  const firstValue = entries.shift()[1];
+
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{firstValue}</p>
+      {entries.map((entry) => {
+        return <p className="tile" key={entry[0] + Date.now()}><em>{entry[0]}: </em>{entry[1]}</p>
+      })}
     </div>
   );
 };
