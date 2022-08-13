2c0c607357398f2f178bddbbb0eed474f707bbe7
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-08-13 13:45:14 +0100

    Manage inputs via state

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -12,12 +12,18 @@ export const ContactForm = ({
   return (
     <form onSubmit={handleSubmit}> 
       <label htmlFor="name">Name: </label>
-      <input type="text" name="name" id="name" />
+      <input type="text" name="name" id="name"
+        onChange={setName}
+      />
       <label htmlFor="phone">Phone Number: </label>
       <input type="tel" name="phone" id="phone"
-        pattern="" />
+        onChange={setPhone}
+        pattern="[0-9]{11}" title="11 digits long"
+      />
       <label htmlFor="email">Email Address: </label>
-      <input type="email" name="email" id="email" />
+      <input type="email" name="email" id="email"
+        onChange={setEmail}
+      />
       <input type="submit" value="Submit" />
     </form>
   );
