a430e40fef12b604a9284873332fd31dccba80df
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-08-13 12:43:35 +0100

    Plan tests

---
+++ src/components/contactForm/contactForm.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { ContactForm } from "./ContactForm";
+
+test.todo("Renders a form with onSubmit attribute set");
+
+test.todo("Renders three managed input elements");
+
+test.todo("Renders a submit button");
+
+test.todo("Does not accept incorrect phone numbers");
