3edf3f783a37082c53791efa3931e81ae001c10e
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-08-13 10:59:35 +0100

    Implement first test

--- src/components/tile/Tile.test.js
+++ src/components/tile/Tile.test.js
@@ -1,10 +1,21 @@
+import { render, getbyText } from "@testing-library/react"
+
 const mockData = {
-  title: "Mock Appointment Title",
+  title: "Mock Appointment",
   date: "1st January 1970",
-  time: "6am"
+  time: "6am",
+  description: "Diplomatic meeting with the mock turles"
 }
 
-test.todo('Displays all object contents')
+const mockDataKeys = mockData.keys();
+
+test('Displays passed object values, once each', () => {
+  render("<Tile display={mockData} />")
+  for (let i = 0; i < mockDataKeys.length; i++) {
+    const expectedText = mockData.mockDataKeys[i]
+    expect(getbyText(expectedText)).toBe(true)
+  }
+})
 
 test.todo('Gives first item class "tile-title"')
 
