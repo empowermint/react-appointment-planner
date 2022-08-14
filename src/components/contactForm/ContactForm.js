5fb827a1deb8b5eb19ccdca29b6fcbc0d592b429
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-08-14 15:13:36 +0100

    Refactor
    - Nest inputs in label to avoid messy id attributes
    
    - Fully manage values with state

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -11,19 +11,22 @@ export const ContactForm = ({
 }) => {
   return (
     <form onSubmit={handleSubmit}> 
-      <label htmlFor="name">Name: </label>
-      <input type="text" name="name" id="name"
-        onChange={setName}
-      />
-      <label htmlFor="phone">Phone Number: </label>
-      <input type="tel" name="phone" id="phone"
-        onChange={setPhone}
-        pattern="[0-9]{11}" title="11 digits long"
-      />
-      <label htmlFor="email">Email Address: </label>
-      <input type="email" name="email" id="email"
-        onChange={setEmail}
-      />
+      <label>Name: 
+        <input type="text" name="name" value={name}
+          onChange={setName}
+        />
+      </label>
+      <label>Phone Number: 
+        <input type="tel" name="phone" value={phone}
+          onChange={setPhone}
+          pattern="[0-9]{11}" title="11 digits long"
+        />
+      </label>
+      <label>Email Address: 
+        <input type="email" name="email" value={email}
+          onChange={setEmail}
+        />
+      </label>
       <input type="submit" value="Submit" />
     </form>
   );
