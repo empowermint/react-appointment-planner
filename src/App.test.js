f575836315489789b17c75ff7dceddc3e68c6d6d
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-03-04 11:29:11 +0000

    Test contact form

--- src/App.test.js
+++ src/App.test.js
@@ -27,3 +27,30 @@ describe("app navigation", () => {
     expect(appointmentsPage).toBeInTheDocument();
   })
 })
+
+test("adds valid contact to display list", () => {
+  render(<BrowserRouter><App /></BrowserRouter>)
+
+  // User navigates to contacts form and inputs valid contact:
+
+  const contactsLink = screen.getByRole("link", {name: "Contacts"})
+  userEvent.click(contactsLink)
+
+  const nameInput = screen.getByLabelText(/Name/i)
+  userEvent.type(nameInput, "Mock Turtle Society Secretary")
+
+  const phoneInput = screen.getByLabelText(/Phone/i)
+  userEvent.type(phoneInput, "01311234567")
+
+  const emailInput = screen.getByLabelText(/Email/i)
+  userEvent.type(emailInput, "secretary@mockturtles.io")
+
+  const submitButton = screen.getByRole("button")
+  userEvent.click(submitButton)
+
+  // New contact information renders:
+
+  expect(screen.getByText("Mock Turtle Society Secretary")).toBeInTheDocument()
+  expect(screen.getByText("01311234567")).toBeInTheDocument()
+  expect(screen.getByText("secretary@mockturtles.io")).toBeInTheDocument()
+})
